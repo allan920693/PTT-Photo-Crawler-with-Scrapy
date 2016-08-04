531beba8b09292c51d70c3e20297d8a5fb072c76
allan920693 <allan920693@yahoo.com.tw>
2016-08-04 16:36:31 +0800

    Add files via upload

---
+++ items.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# http://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class PostItem(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    title = scrapy.Field()
+    author = scrapy.Field()
+    date = scrapy.Field()
+    content = scrapy.Field()
+    comments = scrapy.Field()
+    score = scrapy.Field()
+    url = scrapy.Field()
+    incontent_url = scrapy.Field()
+    incontent_url_type = scrapy.Field()
